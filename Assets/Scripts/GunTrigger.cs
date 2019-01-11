feaf34bb3635733c5633e6e307294ca7df833bb7
yuuu <yuuto_ose@yahoo.co.jp>
2019-01-11 19:18:05 +0900

    Initial commit

---
+++ Assets/Scripts/GunTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GunTrigger : MonoBehaviour {
+
+	public Transform transform;
+	public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+		if (GetComponent<VRTK_ControllerEvents>() == null)
+			return;
+		GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(DoTriggerPressed);
+	}
+
+	private void DoTriggerPressed(object sender, ControllerInteractionEventArgs e)
+	{
+		GameObject create = Instantiate(prefab, transform);
+		ParticleSystem particle = create.GetComponent<ParticleSystem>();
+		Destroy(create, particle.main.duration);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ParticleCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnParticleCollision(GameObject obj)
+	{
+		Destroy(obj);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
